9ae76e9e51c97814e258a6affe316c6e10450c14
Sabrina Sumona <snsbauet04@gmail.com>
2021-07-17 19:24:48 +0600

    Route and Query Parameters

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,7 +9,13 @@ class HomeController extends Controller
   public function index(){
       return view('welcome');
   }
-  public function show(){
-      return 'Show function is called';
+  public function show(Request $request){
+      // return 'Show function is called';
+      $userid= $request->userid;
+      $age= $request->age;
+      $username= request('username');
+      $products= array('table', 'chair', 'stool');
+      // return view('homepage', ['products'=>$products, 'username'=>$username, 'userid'=>$userid, 'age'=>$age]);
+      return view('homepage',compact('userid','age','username','products'));
   }
 }
