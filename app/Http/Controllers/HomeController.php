e881f1bc12a942ab53d7085553b66a578c217398
Sabrina Sumona <snsbauet04@gmail.com>
2021-07-18 18:26:56 +0600

    MySQL To Eloquent (Read Operation)

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,12 +13,14 @@ class HomeController extends Controller
 
   public function show(Request $request){
       // // return 'Show function is called';
+
       // $userid= $request->userid;
       // $age= $request->age;
       // $username= request('username');
       // $products= array('table', 'chair', 'stool');
       // // return view('homepage', ['products'=>$products, 'username'=>$username, 'userid'=>$userid, 'age'=>$age]);
       // return view('homepage',compact('userid','age','username','products'));
+
       // $products = Product::all();
       // $products = Product::find(1);
       // $id = 2;
@@ -27,13 +29,26 @@ class HomeController extends Controller
       // $products= Product::orderBy('price', 'desc')->get();
       // $products= Product::orderBy('price', 'asc')->get();
       // dd($products);
-      Product::create([
-          'id'=>4,
-          'name'=>'mug',
-          'quantity'=>1000,
-          'price'=>250,
-          'color' =>'blue'
-      ]);
-      return 'Successfullly Executed!';
+      // Product::create([
+      //     'id'=>4,
+      //     '
+      // name'=>'mug',
+      //     'quantity'=>1000,
+      //     'price'=>250,
+      //     'color' =>'blue'
+      // ]);
+      // return 'Successfullly Executed!';
+
+      // $readOperation = Product::all();
+      // $readOperation = Product::select('*')->get();
+      // $readOperation = Product::select('name','price')->get();
+      // $readOperation = Product::where('price','>', 300)->get();
+      // $readOperation = Product::select('name','price','quantity')->where('price','>', 250)->get();
+      // $readOperation = Product::select('name','price','quantity')->where('id','=', 1)->get();
+      // $readOperation = Product::select('name','price','quantity')->where('id','>=', 2)->get();
+      // $readOperation = Product::select('name','price','quantity')->where('color','=', 'blue')->get();
+      $readOperation = Product::select('name','price','quantity')->where('price','<', 250)->first();
+      // dd($readOperation);
+      return $readOperation;
   }
 }
